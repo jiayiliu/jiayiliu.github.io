cbd00578f4530aa4a6c62d99c68805179292ad0f
Jiayi Liu <jiayi.uiuc@gmail.com>
2019-10-29 23:31:18 -0700

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -138,6 +138,7 @@ NAVIGATION_LINKS = {
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
         ("/rss.xml", "RSS feed"),
+        ("https://colab.research.google.com/github/jiayiliu/jiayiliu.github.io/blob/src/posts/index.ipynb", "Google Colab")
     ),
 }
 
@@ -567,7 +568,7 @@ FRONT_INDEX_HEADER = {
 # If monthly archives or full archives are created, adds also one archive per day
 # CREATE_DAILY_ARCHIVE = False
 # Create previous, up, next navigation links for archives
-# CREATE_ARCHIVE_NAVIGATION = False
+CREATE_ARCHIVE_NAVIGATION = True
 # Final locations for the archives are:
 # output / TRANSLATION[lang] / ARCHIVE_PATH / ARCHIVE_FILENAME
 # output / TRANSLATION[lang] / ARCHIVE_PATH / YEAR / index.html
